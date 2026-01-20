41d08824e7cf3dc1c6f87e582867fa51757f756f
Angelos Tsangarides <angelos@tsangarides.net>
2026-01-20 16:44:13 +0000

    Initial commit. Added project file structure, updated README.md.

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+// Define document (i.e. tab) title
+$DOCUMENT_TITLE = "Home";
+
+// Include page head
+require_once 'partials/head.php';
+?>
+
+<!-- Markup goes here -->
+
+<?php
+// Include page footer and closing tags
+require_once 'partials/footer.php';
+?>
+
