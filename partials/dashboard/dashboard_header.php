033b6e03a03edad389387f0250be70e4cfc70ed9
Angelos Tsangarides <angelos@tsangarides.net>
2026-01-24 15:45:15 +0000

    Implemented overarching dashboard UI.

--- partials/dashboard/dashboard_header.php
+++ partials/dashboard/dashboard_header.php
@@ -4,7 +4,19 @@
     </div>
 
     <div class="dashboard-header__account">
-        <span class="dashboard-header__account__name">[Username]</span>
+        <span class="dashboard-header__account__name">
+            <?php
+                // Get current user object
+                $user = \TTE\App\Auth\Authenticator::getCurrentUserSubclass();
+
+                // Display 'seller name' or 'customer username' depending on account type.
+                if ($user instanceof \TTE\App\Model\Seller) {
+                    echo $user->getName();
+                } else if ($user instanceof \TTE\App\Model\Customer) {
+                    echo $user->getUsername();
+                }
+            ?>
+        </span>
         <a href="/backend/API/Auth/logout.php" class="button dashboard-header__account_logout">Log Out</a>
     </div>
 </header>
