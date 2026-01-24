59233d5f02a453b63a3224f1df73636410e5b3ee
Angelos Tsangarides <angelos@tsangarides.net>
2026-01-24 14:24:05 +0000

    Began implementation of dashboard UI.

---
+++ dashboard.php
@@ -0,0 +1,23 @@
+<?php
+// Define document (i.e. tab) title
+$DOCUMENT_TITLE = "Dashboard";
+
+// Include page head
+require_once 'partials/head.php';
+
+// Include dashboard header (i.e. 'title bar')
+require_once 'partials/dashboard/dashboard_header.php';
+
+?>
+
+<div class="dashboard-wrapper">
+    <div class="dashboard">
+
+    </div>
+</div>
+
+<?php
+// Include page footer and closing tags
+require_once 'partials/footer.php';
+?>
+
---
+++ partials/dashboard/dashboard_header.php
@@ -0,0 +1,10 @@
+<header class="dashboard-header">
+    <div class="dashboard-header__branding">
+        <span class="dashboard-header__branding__title">Team Project App</span>
+    </div>
+
+    <div class="dashboard-header__account">
+        <span class="dashboard-header__account__name">[Username]</span>
+        <a href="/backend/API/Auth/logout.php" class="button dashboard-header__account_logout">Log Out</a>
+    </div>
+</header>
