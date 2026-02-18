2439334d217bb0044c87f962d0ac3b7dcff68461
Angelos Tsangarides <angelos@tsangarides.net>
2026-02-18 21:33:46 +0000

    Created CustomerTest.php

---
+++ backend/Tests/Model/CustomerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace TTE\App\Tests\Model;
+
+use PHPUnit\Framework\TestCase;
+// Global for session to run test
+$_SESSION = array();
+
+
+// Class testing functions of the Customer class
+class CustomerTest extends TestCase {
+
+}
