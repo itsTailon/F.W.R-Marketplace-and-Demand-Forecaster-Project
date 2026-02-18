b6cd3e439a9585742df563dc73aafca84dc9828e
chas <123321721+chassinat0r@users.noreply.github.com>
2026-02-18 15:05:04 +0000

    Add comments

--- assets/js/bundle_form.js
+++ assets/js/bundle_form.js
@@ -1,25 +1,33 @@
-var count = 0;
+var count = 0; // Used to determine the allergen index
 
+// When mouse hovers over the delete button of an allergy, change its icon to the normal bin icon
 $(document).on('mouseenter', '.allergen-list__item__delete-btn', function () {
     $(this).find("img").attr("src", "/assets/icons/bin.png");
 });
 
+// When mouse stops hovering over the delete button of an allergy, change its icon to the faded bin icon
 $(document).on('mouseleave', '.allergen-list__item__delete-btn', function () {
     $(this).find("img").attr("src", "/assets/icons/bin_faded.png");
 });
 
+// When an allergen is changed, check if it should have the disabled class applied
 $(document).on('change', '.allergen-list__item__selector', function() {
-    if ($(this).val() !== "") {
-        $(this).removeClass("disabled");
-    } else {
-        $(this).addClass("disabled");
+    if ($(this).val() !== "") { // If the allergen value is not the placeholder
+        $(this).removeClass("disabled");  // Do not show as disabled
+    } else { // If the allergen value is the placeholder
+        $(this).addClass("disabled"); // Show as disabled
     }
 });
 
+// When an allergen delete button is clicked, delete the allergen from the list
 $(document).on('click', '.allergen-list__item__delete-btn', function () {
     $(this).parent().remove();
 });
 
+/* addAllergen
+Add a new allergen with drop-down selector and delete button to the list.
+Index increments each time so each entry can be distinguished.
+*/
 function addAllergen() {
     $(".allergen-list").append(`
         <li class="allergen-list__item" index="${count}">
@@ -45,10 +53,12 @@ function addAllergen() {
     `);
     count++;
 }
-$("#add-allergen-btn").click(addAllergen);
 
-addAllergen();
+$("#add-allergen-btn").click(addAllergen); // When the add allergen button is clicked, add a new allergen
 
+addAllergen(); // Run once when the page loads
+
+// Move the labels in the form so they align with their fields
 $('div.edit-form__field').each(function () {
     const label = $(this).find('label');
     if ($(this).find('textarea').length > 0) {
