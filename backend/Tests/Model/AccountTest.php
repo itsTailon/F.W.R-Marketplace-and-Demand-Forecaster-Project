745690222426544a4817da2a16c0ac6b50266b75
Angelos Tsangarides <angelos@tsangarides.net>
2026-02-18 21:54:38 +0000

    Added unit tests to CustomerTest. Created AccountTest.php

---
+++ backend/Tests/Model/AccountTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TTE\App\Tests\Model;
+
+use PHPUnit\Framework\TestCase;
+
+class AccountTest extends TestCase {
+
+}
--- backend/Tests/Model/CustomerTest.php
+++ backend/Tests/Model/CustomerTest.php
@@ -4,11 +4,58 @@
 namespace TTE\App\Tests\Model;
 
 use PHPUnit\Framework\TestCase;
+use TTE\App\Model\Customer;
+use TTE\App\Model\DatabaseException;
+use TTE\App\Model\MissingValuesException;
+
 // Global for session to run test
 $_SESSION = array();
 
 
 // Class testing functions of the Customer class
 class CustomerTest extends TestCase {
+
+    public function testLoad(): void {
+        // Create customer to test loading
+        $customer1 = Customer::create([
+            'username' => 'testLoadCustomer1',
+            'password' => 'password',
+            'email' => 'testLoadCustomer1@example.com',
+        ]);
+
+        // Test loading of actual customer record (i.e. valid ID)
+        $this->assertEquals($customer1, Customer::load($customer1->getUserID()));
+
+        // Cleanup
+        Customer::delete($customer1->getUserID());
+
+        // Ensure that the method throws a DatabaseException if no customer exists with the given ID
+        $thrown = false;
+        try {
+            Customer::load($customer1->getUserID()); // $customer1 was deleted, so its ID is no longer valid
+        } catch (DatabaseException $e) {
+            $thrown = true;
+        }
+        $this->assertTrue($thrown);
+    }
+
+    public function testExistsWithID(): void {
+        // Create customer to test loading
+        $customer1 = Customer::create([
+            'username' => 'testLoadCustomer1',
+            'password' => 'password',
+            'email' => 'testLoadCustomer1@example.com',
+        ]);
+
+        // Test method on valid customer ID
+        $this->assertTrue(Customer::existsWithID($customer1->getUserID()));
+
+        // Cleanup
+        Customer::delete($customer1->getUserID());
+
+        // Test method on invalid (non-existent) customer ID
+        $this->assertFalse(Customer::existsWithID($customer1->getUserID())); // $customer1 was deleted, so its ID is no longer valid
+    }
+
 
 }
