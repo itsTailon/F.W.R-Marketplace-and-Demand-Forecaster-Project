61907d3d5ce632e5b187816a000b456b9ca8abdc
whm208 <whm208@exeter.ac.uk>
2026-02-18 22:19:37 +0000

    Implement test for account deletion
    Added a test for deleting an account in AccountTest.

--- backend/Tests/Model/AccountTest.php
+++ backend/Tests/Model/AccountTest.php
@@ -5,5 +5,22 @@ namespace TTE\App\Tests\Model;
 use PHPUnit\Framework\TestCase;
 
 class AccountTest extends TestCase {
+    /**
+     * @throws DatabaseException|NoSuchAccountException|MissingValuesException
+     */
+    public function testDeleteAccount()
+    {
+        // Account fields for create() methods
+        $accountFields = array(
+            "userID" => "2020",
+            "email" => "testAcc@gmail.com",
+            "accountType" => "customer",
+        );
 
+        // Creating required Account object
+        $account = Account::create($accountFields);
+
+        // Delete account
+        Account::delete($account->getUserID());
+    }
 }
