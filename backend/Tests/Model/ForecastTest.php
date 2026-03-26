a650351209ab5d5493e83577c489bb75b24321d0
Tom Staley <tomstaley06@gmail.com>
2026-03-26 04:32:01 +0000

    ensured forecast tests can all run automatically

--- backend/Tests/Model/ForecastTest.php
+++ backend/Tests/Model/ForecastTest.php
@@ -330,8 +330,6 @@ class ForecastTest extends TestCase
             'quantity' => 100
         ]);
 
-
-
         Reservation::create([
             'purchaserID' => $purchaser->getUserID(),
             'bundleID' => $bundle1->getID(),
